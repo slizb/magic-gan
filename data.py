df38005acba578bd25eb9a467774b028fc63d7a7
Bradley Sliz <bradsliz@gmail.com>
2018-06-27 10:29:26 -0500

    refactoring to exclude test data, which isn't needed for the generator

--- data.py
+++ data.py
@@ -1,25 +1,11 @@
 
 import pandas as pd
-import imageio
 import numpy as np
 from keras.preprocessing.image import load_img, img_to_array
 
 IMG_DIR = 'img/cropped/'
 SIZE = (210, 210, 3)
 
-
-# def load_frame(frame):
-#     x = []
-#     y = []
-#     for ix, row in frame.iterrows():
-#         img_path = IMG_DIR + row['img_file']
-#         img = imageio.imread(img_path)
-#         label = row['land_type']
-#         x.append(img)
-#         y.append(label)
-#     x = np.asarray(x)
-#     y = np.asarray(y)
-#     return x, y
 
 def load_frame(frame):
     x = []
@@ -37,9 +23,6 @@ def load_frame(frame):
 
 
 def load_data():
-    train = pd.read_csv('train.csv')
-    test = pd.read_csv('test.csv')
+    train = pd.read_csv('df.csv')
     x_train, y_train = load_frame(train)
-    x_test, y_test = load_frame(test)
-    return x_train, y_train, x_test, y_test
-
+    return x_train, y_train
